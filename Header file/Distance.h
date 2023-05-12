730fd61f19c6a1e68cbf614fba05ad3d8668ba0d
Jan Farag <132142327+janfarag@users.noreply.github.com>
2023-05-13 01:17:49 +0300

    Add files via upload

---
+++ Header file/Distance.h
@@ -0,0 +1,10 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+
+double ToDegree(double angle);
+
+double ToRadian(double angle);
+
+double CalculateDistance(double latitude_1, double longitude_1, double latitude_2, double longitude_2);
+
+#endif // !DISTANCE_H
